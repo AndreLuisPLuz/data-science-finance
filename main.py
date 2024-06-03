02e3c417ab59aa06df8723a74be30a5909eae32d
AndreLuisPLuz <andreluispluz@gmail.com>
2024-06-03 20:18:20 -0300

    feat: STL analysis, monthly and quarterly average returns added

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+from statsmodels.tsa.seasonal import STL
 
 if __name__ == "__main__":
+    # Organizando os dados por data
     data = pd.read_csv('stocks_prediction.csv')
 
     data['Date'] = pd.to_datetime(data['Date'])
 
     data = data.sort_values('Date')
 
+    # Estimando os ganhos diários
     data['Return'] = data['Close'].pct_change()
 
+    # Plotando a série histórica completa
     plt.figure(figsize=(12, 6))
     for stock in data['Stock Name'].unique():
         stock_data = data[data['Stock Name'] == stock]
@@ -20,3 +24,34 @@ if __name__ == "__main__":
     plt.title('Stock Prices Over Time')
     plt.legend()
     plt.show()
+
+    # Decompondo a série história de acordo com uma ação especícia com STL
+    stock_data = data[data['Stock Name'] == 'PETR4.SA']
+    stock_data = stock_data.set_index('Date')
+
+    stl = STL(stock_data['Close'], period=365, seasonal=731)
+    result = stl.fit()
+
+    result.plot()
+    plt.show()
+
+    # Conseguindo os retornos mensais
+    data['Month'] = data['Date'].dt.month
+    monthly_returns = data.groupby(['Month', 'Stock Name'])['Return'].mean().unstack()
+
+    monthly_returns.plot(kind='bar', figsize=(12, 6))
+    plt.title('Average Monthly Returns')
+    plt.xlabel('Month')
+    plt.ylabel('Average Return')
+    plt.show()
+
+    # Conseguindo os retornos trimestrais
+    data['Quarter'] = data['Date'].dt.quarter
+    quarterly_returns = data.groupby(['Quarter', 'Stock Name'])['Return'].mean().unstack()
+
+    quarterly_returns.plot(kind='bar', figsize=(12, 6))
+    plt.title('Average Quarterly Returns')
+    plt.xlabel('Quarter')
+    plt.ylabel('Average Return')
+    plt.show()
+
